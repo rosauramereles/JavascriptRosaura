e04972b0dfc281ea5a571ae3db5d721ba1a15b38
rosauramereles <rosauramereles15@gmail.com>
2026-05-29 14:19:05 -0300

    Actividad6-Pares e Impares

---
+++ actividad6.js
@@ -0,0 +1,11 @@
+//Actividad6-Pares e Impares 
+
+let numero = 0;
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} es un numero par`);
+    } else {
+        console.log(`${i} es un numero impar`);
+    }
+}
