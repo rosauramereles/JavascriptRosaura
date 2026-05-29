11281d5ddb96cd50dc9cf2351b941c36c68655de
rosauramereles <rosauramereles15@gmail.com>
2026-05-29 10:39:25 -0300

    Actividad2-Aprobado o Desaprobado

---
+++ actividad2.js
@@ -0,0 +1,9 @@
+//Actividad 2-Aprobado o Desaprobado 
+let nota = 7; // Variable para almacenar la nota del estudiante
+
+//condicional para  ver si el estudiante aprobó o desaprobó
+if (nota >= 6) {
+    console.log("Aprobado");
+} else {
+    console.log("Desaprobado");
+}
