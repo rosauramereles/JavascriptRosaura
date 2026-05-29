ffcb586fca547f2b1a93863baf8c08f73e42c94f
rosauramereles <rosauramereles15@gmail.com>
2026-05-29 10:12:44 -0300

    Actidad 1-Datos Personales

---
+++ app.js
@@ -0,0 +1,8 @@
+// Actividad 1-Datos Personales
+const nombre = "Rosaura";
+const apellido = "Mereles";
+let edad = 20;
+const carrera = "Desarrollo de Software";
+let cursando = true;
+ //Mostrando mensajes por interpolación de string
+console.log(`Hola, mi nombre es ${nombre} ${apellido}, tengo ${edad} años y estudio ${carrera}. Actualmente estoy cursando la carrera: ${cursando}.`);
