5ce7724227cc0baf84e47fca075bca7f44408edc
rosauramereles <rosauramereles15@gmail.com>
2026-05-29 11:48:45 -0300

    Actividad4-Tabla de Multiplicar

---
+++ actividad4.js
@@ -0,0 +1,7 @@
+// Actividad4-Tabla de multiplicar 
+const numero = 7; 
+// for para generar la tabla de multiplicar del numero 7; es el bucle  
+
+for(let i =1; i <=10; i++) {
+    console.log (`${numero} x ${i} = ${numero * i}`);
+}
