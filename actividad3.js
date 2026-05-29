70656f68b9001701b11c5b6a14aa2dd940903ac0
rosauramereles <rosauramereles15@gmail.com>
2026-05-29 11:13:34 -0300

    Actividad3-Clasificación de edad

---
+++ actividad3.js
@@ -0,0 +1,10 @@
+//Actividad3-Clasificación de edades 
+let edad = 15; // Esto se encarga para guardar la edad de una persona, es decir es la variable que almecena la edad
+// condicional para clasificar la edad 
+if (edad >= 13) {
+    console.log("Niño");
+} else if (edad >= 13 && edad <= 17) {
+    console.log("Adolescente");
+} else if (edad >= 18) {
+    console.log("Adulto");
+}
